2dffd57b752980e7f5fc2af101773f63b4535b48
ShwRat410 <shwetaratnawat410@gmail.com>
2025-03-19 20:25:49 +0000

    Extracting error data and throwing responses

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import PageContent from '../components/PageContent'
+import { useRouteError } from 'react-router-dom'
 
 export default function ErrorPage() {
+
+  const error = useRouteError()
+
+  let title = "An error occured"
+  let message = "Something went wrong" 
+  console.log(error.status)
+
+  if(error.status===500){
+    message = JSON.parse(error.data).message
+  }
+
+  if(error.status===404){
+    message = "Could not find resource or page"
+  }
+
   return (
     <div>
-      Error has occured in events
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
     </div>
   )
 }
--- frontend/src/pages/Event.js
+++ frontend/src/pages/Event.js
@@ -15,9 +15,10 @@ export default function EventPage() {
 }
 
 export async function loader(){
-  const response = await fetch("http://localhost:8080/eventss")
+  const response = await fetch("http://localhost:8080/events")
   if(!response.ok){
-    throw ({message:"An error occured"})
+//    throw ({message:"An error occured"})
+    throw new Response(JSON.stringify({message:"Could not fetch events"}),{status:500})
   }
   else{
     const resData = await response.json()
